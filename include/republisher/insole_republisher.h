1c51f9fae1cb533193c984baecd49b075697aaaa
frekle <frekle@kth.se>
2024-01-09 16:36:25 +0000

    untested big removal

--- include/republisher/insole_republisher.h
+++ include/republisher/insole_republisher.h
@@ -11,7 +11,7 @@
 #include "insole_msgs/InsoleSensorStamped.h"
 #include "republisher/republisher.h"
 #include "ros/duration.h"
-#include "ros/message_traits.h"
+#include "ros/node_handle.h"
 #include "ros/time.h"
 #include <optional>
 
@@ -19,56 +19,99 @@
 class InsoleRepublisherWithTimeCorrection: public Republisher<insole_msgs::InsoleSensorStamped>
 {
 	public:
-	InsoleRepublisherWithTimeCorrection()
-	{
-		nh.param<int>("maximum_delay_before_refresh",maximum_delay_before_refresh, 10); //in ms
-		int insole_init_time = 0;
-		nh.param<int>("insole_time_when_synchronized", insole_init_time,0);
-		insole_time_when_synchronized = static_cast<uint32_t>(insole_init_time);
-		//now i need to get the ros:time when synchronized from a parameter which is a bit trickier
-		int sync_time_secs, sync_time_nsecs;
-		nh.param<int>("sync_time_secs", sync_time_secs, 1668695814);
-		nh.param<int>("sync_time_nsec", sync_time_nsecs, 0);
-		ros_time_when_synchronized = ros::Time{static_cast<uint32_t>(sync_time_secs),static_cast<uint32_t>(sync_time_nsecs)};
-		Republisher();
-
-	}
-	std::optional<insole_msgs::InsoleSensorStamped> last_gotten_value;
-	int maximum_delay_before_refresh = 10;
-	ros::Time ros_time_when_synchronized;
-	uint32_t insole_time_when_synchronized; 	
-	int ros_time_to_insole_time (ros::Time some_time)
-	{
-		// the times when synchronized should be some class variable i think. 
-		//
-		auto time_diff = some_time - ros_time_when_synchronized;
-		return time_diff.toNSec()/1e6 + insole_time_when_synchronized;
-	}
-	std::optional<insole_msgs::InsoleSensorStamped> get_latest()
-	{
-		//I have a last_gotten_value which is older.
-		std::optional<insole_msgs::InsoleSensorStamped> output_value;
-		auto el = Republisher::get_latest();
-		//this removes the value from the deck. 
-		//check if it is time to actually use this value
-		//
-		//compare it with the clock. if the clock is older/newer idk, my head is a mush
-		bool choose_older_value_condition = ros_time_to_insole_time(ros::Time::now()) - el.value().time.data > maximum_delay_before_refresh;
-		if(choose_older_value_condition)
+		InsoleRepublisherWithTimeCorrection()
 		{
-			//then i need to put back the get latest
-			input_queue.push_front(el.value());
-			output_value = last_gotten_value;
+			nh = ros::NodeHandle("~");
+			nh.param<int>("maximum_delay_before_refresh",maximum_delay_before_refresh, 10); //in ms
+			int insole_init_time = 0;
+			nh.param<int>("insole_time_when_synchronized", insole_init_time,0);
+			insole_time_when_synchronized = static_cast<uint32_t>(insole_init_time);
+			//now i need to get the ros:time when synchronized from a parameter which is a bit trickier
+			int sync_time_secs, sync_time_nsecs;
+			nh.param<int>("sync_time_secs", sync_time_secs, 1668695814);
+			nh.param<int>("sync_time_nsec", sync_time_nsecs, 0);
+			ros_time_when_synchronized = ros::Time{static_cast<uint32_t>(sync_time_secs),static_cast<uint32_t>(sync_time_nsecs)};
+			ROS_INFO_STREAM("Ros time when synchronized" << ros_time_when_synchronized);
+			ROS_INFO_STREAM("insole_time_when_synchronized" << insole_time_when_synchronized);
+			Republisher();
+
 		}
-		else
+		std::optional<insole_msgs::InsoleSensorStamped> last_gotten_value;
+		int maximum_delay_before_refresh = 10;
+		ros::Time ros_time_when_synchronized;
+		uint32_t insole_time_when_synchronized; 	
+		std::optional<uint32_t> first_insole_time_this_measurement;
+		ros::Duration this_measurement_time_offset;
+		int ros_time_to_insole_time (ros::Time some_time)
 		{
-			output_value = el;
+			// the times when synchronized should be some class variable i think. 
+			//
+			auto time_diff = some_time - ros_time_when_synchronized;
+			ROS_INFO_STREAM("Time right now - time when I know I was synchronized: " << time_diff);
+			int predicted_insole_time = time_diff.toNSec()/1e6 + insole_time_when_synchronized;
+			ROS_INFO_STREAM("predicted_insole_time (aka that number that should appear as the time field, but maybe it isn't): "<<predicted_insole_time);
+			return predicted_insole_time;
 		}
+		std::optional<insole_msgs::InsoleSensorStamped> get_latest() 
+		{
+			//I have a last_gotten_value which is older.
+			std::optional<insole_msgs::InsoleSensorStamped> output_value;
+			auto el = Republisher::get_latest();
+			//this removes the value from the deck. 
+			//check if it is time to actually use this value
+			//
+			//compare it with the clock. if the clock is older/newer idk, my head is a mush
+			if (el.has_value())
+			{
+				auto real_insole_time = el.value().time.data;
+				if(!first_insole_time_this_measurement)
+				{
+					first_insole_time_this_measurement = real_insole_time;
+					this_measurement_time_offset = ros::Time::now() - ros_time_when_synchronized;
+				}
 
-		last_gotten_value = output_value; //because of reasons.
-		return el;
-	}
+				//ROS_WARN_STREAM("real_insole_time:"<<real_insole_time);
+				int insole_time_difference =ros_time_to_insole_time(ros::Time::now()) - real_insole_time; 
+				if(insole_time_difference < 0) //this means that the insole should already have started, right?
+				{
+					ROS_FATAL_STREAM("insole time difference cannot be negative!!\ninsole_time_difference:" << insole_time_difference);
+					//return el;
+				}
+				bool choose_older_value_condition = insole_time_difference < maximum_delay_before_refresh;
+				if(choose_older_value_condition)
+				{
+					ROS_INFO_STREAM("chosen older value");
+					//then i need to put back the get latest
+					input_queue.push_front(el.value());
+					output_value = last_gotten_value;
+				}
+				else
+				{
+					output_value = el;
+					//but we also need to correct the time, right?
+					output_value.value().header.stamp = convert_insole_to_ros_time(real_insole_time); 
+				}
 
+				last_gotten_value = output_value; //because of reasons.
+			}
+			return output_value;
+		}
+		ros::Time convert_insole_to_ros_time(int time_insole)
+		{
+			int time_diff = time_insole - insole_time_when_synchronized;
+			if (time_diff<0)
+			{
+
+				ROS_FATAL_STREAM("current insole time is smaller than when insole was synchronized?!?!");
+			}
+			ROS_INFO_STREAM("time_diff:" << time_diff);
+			auto offset_time = ros::Duration().fromSec(double(time_diff)/1000); 
+			ROS_INFO_STREAM("duration to add to time" <<offset_time);
+			auto converted_time =ros_time_when_synchronized+offset_time; 
+			ROS_WARN_STREAM("converted time:" <<converted_time);
+			return converted_time; 
+
+		}
 
 };
 
--- include/republisher/republisher.h
+++ include/republisher/republisher.h
@@ -1,7 +1,6 @@
 #ifndef REPUBLISHER_HEADER_29052023
 #define REPUBLISHER_HEADER_29052023
 #include "geometry_msgs/WrenchStamped.h"
-#include "insole_msgs/InsoleSensorStamped.h"
 #include "republisher/filter.h"
 #include "ros/node_handle.h"
 #include "ros/publisher.h"
@@ -14,9 +13,7 @@
 
 struct PublicationBuffer
 {
-	geometry_msgs::WrenchStamped w_oversampled;
-	geometry_msgs::WrenchStamped w_filtered;
-	geometry_msgs::TransformStamped t_oversampled;
+	geometry_msgs::WrenchStamped w;
 	geometry_msgs::TransformStamped t_filtered;
 };
 
@@ -30,7 +27,6 @@ class Republisher
 		ros::Duration delay;
 		tf::TransformBroadcaster tf;
 		bool debug_publish_zero_cop, debug_publish_fixed_force;
-		ros::Publisher wrench_publisher;
 		ros::Publisher wrench_publisher_f;
 
 		RosOpenSimRTFilter* wfilter;
@@ -43,10 +39,7 @@ class Republisher
 			nh.param<int>("maximum_queue_length", maximum_queue_length, 1000);
 			nh.param<int>("input_queue_length", input_queue_length, 10);
 			sub = nh.subscribe("input", 10, &Republisher::callback, this);
-			pub = nh.advertise<insole_msgs::InsoleSensorStamped>("output", input_queue_length);
-			double rate;
-			nh.param<double>("rate", rate, 100);
-			r = new ros::Rate(rate);
+			pub = nh.advertise<T>("output", input_queue_length);
 			double delay_seconds;
 			nh.param<double>("wrench_delay", delay_seconds, 0); 
 			delay.fromSec(delay_seconds);
@@ -54,7 +47,6 @@ class Republisher
 			nh.param<bool>("debug_publish_zero_cop", debug_publish_zero_cop, false);
 			nh.param<bool>("debug_publish_fixed_force", debug_publish_fixed_force, false);
 		
-			wrench_publisher = nh.advertise<geometry_msgs::WrenchStamped>("wrench_oversampled",1);
 			wrench_publisher_f = nh.advertise<geometry_msgs::WrenchStamped>("wrench_filtered",1);
 
 			ROS_INFO_STREAM("Finished setting up republisher OK.");
@@ -62,7 +54,7 @@ class Republisher
 
 		void run()
 		{
-			std::optional<insole_msgs::InsoleSensorStamped> el_o = get_latest();
+			std::optional<T> el_o = get_latest();
 			if (!el_o)
 			{
 				return;
@@ -84,18 +76,11 @@ class Republisher
 			auto w = geometry_msgs::WrenchStamped();
 			w.header = el.header;
 			w.wrench = el.wrench;
-			w.header.frame_id = el.header.frame_id + "_oversampled";
 			///////wrench_publisher.publish(w);
 			auto st = geometry_msgs::TransformStamped();
 			st.header = el.header;
 			st.header.frame_id = el.ts.header.frame_id;
 			st.transform = el.ts.transform;
-			st.child_frame_id = el.ts.child_frame_id+"_oversampled";
-			/////////tf.sendTransform(st);
-			pb.w_oversampled = w;
-			pb.t_oversampled = st;
-			ROS_DEBUG_STREAM("Sent oversampled values to buffer.");
-			//modify w to its filtered version
 			if (wfilter->publish_filtered)
 			{
 				ROS_DEBUG_STREAM("Calculating filtered values.");
@@ -108,58 +93,38 @@ class Republisher
 				x[5] = st.transform.translation.z;
 				auto x_filtered = wfilter->filter(time, x);
 				//makes sure i always have a valid header even when the filter is not yet ready.
-				auto st_filtered = st;
-				st_filtered.child_frame_id = el.ts.child_frame_id + "_filtered";
+				st.child_frame_id = el.ts.child_frame_id + "_filtered";
 				if (x_filtered.isValid)
 				{
 					ROS_DEBUG_STREAM("Filter is valid");
-					auto w_filtered =w;
-					w_filtered.header.frame_id = st_filtered.child_frame_id; //this is the correct frame for filtered, right?
+					w.header.frame_id = st.child_frame_id; //this is the correct frame for filtered, right?
 					if (debug_publish_fixed_force)
 					{
-						w_filtered.wrench.force = geometry_msgs::Vector3();
-						w_filtered.wrench.force.y = 100;
+						w.wrench.force = geometry_msgs::Vector3();
+						w.wrench.force.y = 100;
 					}
 					else
 					{
-						w_filtered.wrench.force.x = x_filtered.x[0];
-						w_filtered.wrench.force.y = x_filtered.x[1];
-						w_filtered.wrench.force.z = x_filtered.x[2];
+						w.wrench.force.x = x_filtered.x[0];
+						w.wrench.force.y = x_filtered.x[1];
+						w.wrench.force.z = x_filtered.x[2];
 					}
 					ROS_DEBUG_STREAM(x <<"  "<<x_filtered.x);
 					///////wrench_publisher_f.publish(w);
 					if (debug_publish_zero_cop)
 					{
-						st_filtered.transform.translation = geometry_msgs::Vector3();
+						st.transform.translation = geometry_msgs::Vector3();
 					}
 					else
 					{
-						st_filtered.transform.translation.x = x_filtered.x[3];
-						st_filtered.transform.translation.y = x_filtered.x[4];
-						st_filtered.transform.translation.z = x_filtered.x[5];
+						st.transform.translation.x = x_filtered.x[3];
+						st.transform.translation.y = x_filtered.x[4];
+						st.transform.translation.z = x_filtered.x[5];
 					}
-					///////////tf.sendTransform(st_filtered);
-					pb.t_filtered = st_filtered;
-					pb.w_filtered = w_filtered;
-					ROS_DEBUG_STREAM("Sent values to 'buffer' (it's not really a buffer. there is one sample there...).");
-					ROS_DEBUG_STREAM(pb.t_filtered << pb.t_oversampled << pb.w_filtered << pb.w_oversampled);
-					wrench_publisher.publish(pb.w_oversampled);
+					///////////tf.sendTransform(st);
+					pb.t_filtered = st;
+					pb.w = w;
 					// check if the tf trasnformaitons are complete
-					if (pb.t_oversampled.header.frame_id == "")
-					{
-						ROS_ERROR("t_oversampled has no source frame_id");
-					}
-					else
-					{
-						if (pb.t_oversampled.child_frame_id != "")
-						{
-							tf.sendTransform(pb.t_oversampled);
-						}
-						else
-						{
-							ROS_ERROR("oversampled has no child_frame_id!");
-						}
-					}
 					if (pb.t_filtered.header.frame_id == "")
 					{
 						ROS_ERROR("t_filtered has no source frame_id");
@@ -170,7 +135,7 @@ class Republisher
 						{
 							tf.sendTransform(pb.t_filtered);
 							//we will only publish if everything is okay with the transform too. 
-							wrench_publisher_f.publish(pb.w_filtered);
+							wrench_publisher_f.publish(pb.w);
 						}
 						else
 						{
@@ -182,7 +147,7 @@ class Republisher
 			old_time = time;
 		}
 
-		std::optional<insole_msgs::InsoleSensorStamped> get_latest()
+		virtual std::optional<T> get_latest()
 		{
 			if (input_queue.size()>0)
 			{
@@ -193,7 +158,7 @@ class Republisher
 				el.header = h;
 				input_queue.pop_front();
 				ROS_DEBUG_STREAM(el);
-				return std::optional<insole_msgs::InsoleSensorStamped>(el);
+				return std::optional<T>(el);
 			}
 			else
 			{
@@ -214,16 +179,16 @@ class Republisher
 			pub.publish(get_latest());
 		}
 		ros::NodeHandle nh{"~"};
-		std::deque<insole_msgs::InsoleSensorStamped> input_queue;
+		std::deque<T> input_queue;
 	private:
 		ros::Subscriber sub;
 		ros::Publisher pub;
 		int input_queue_length, maximum_queue_length;
-		std::optional<insole_msgs::InsoleSensorStamped> last_value;
+		std::optional<T> last_value;
 		void callback(T msg_insole)
 		{
 			ROS_DEBUG_STREAM("received value!");
-			last_value = std::optional<insole_msgs::InsoleSensorStamped>(msg_insole);
+			last_value = std::optional<T>(msg_insole);
 			input_queue.push_back(msg_insole);
 			while (input_queue.size()>maximum_queue_length)
 			{
